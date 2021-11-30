1293fb23e4cda11eef5fd8ebd6e95afd7f58d908
JasonWandrag <jasonwandrag@hotmail.com>
2021-11-30 23:28:29 +0200

    Make Mongo DB Connection

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv/config");
 
 // Middleware
-app.use();
+// app.use();
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -10,6 +12,11 @@ app.get("/", (req, res) => {
     message: "Welcome",
   });
 });
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () =>
+  console.log("Connected to DB!")
+);
 
 // Start listening to server
 app.listen(3000);
